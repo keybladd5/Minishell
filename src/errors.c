526bedf8c7b21de44e170d29669e836ec058ceb0
Pol Martinez Badenes <polmarti@car11s1.42barcelona.com>
2024-03-11 11:53:57 +0100

    refactor and norm check

--- src/errors.c
+++ src/errors.c
@@ -11,7 +11,79 @@
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-void ft_error_system(int type)
+
+static void	ft_err_red_metachar(int *exit_status, int name, t_token *t_current)
+{
+	ft_putstr_fd("\033[31mminishell: syntax error near unexpected token `", 2);
+	if (!ft_strncmp(t_current->str, ">>", 2) \
+		|| !ft_strncmp(t_current->str, "<<", 2))
+	{
+		ft_putchar_fd(t_current->str[0], 2);
+		ft_putchar_fd(t_current->str[1], 2);
+	}
+	else
+		ft_putchar_fd(t_current->str[0], 2);
+	ft_putstr_fd("'\x1b[0m\n", 2);
+}
+
+static void	ft_err_red_errorfile(int *exit_status, int name, t_token *t_current)
+{
+	char	*tmp_abs_path;
+
+	tmp_abs_path = getcwd(NULL, 0);
+	if (!tmp_abs_path)
+		ft_error_system(MALLOC_ERROR);
+	tmp_abs_path = ft_strjoin_s(tmp_abs_path, "/");
+	if (!tmp_abs_path)
+		ft_error_system(MALLOC_ERROR);
+	tmp_abs_path = ft_strjoin_s(tmp_abs_path, t_current->str);
+	if (!tmp_abs_path)
+		ft_error_system(MALLOC_ERROR);
+	if (access(tmp_abs_path, F_OK) != 0)
+	{
+		ft_putstr_fd("\033[31mminishell: ", 2);
+		ft_putstr_fd(t_current->str, 2);
+		ft_putstr_fd(": No such file or directory\x1b[0m\n", 2);
+	}
+	else
+	{
+		ft_putstr_fd("\033[31mminishell: ", 2);
+		ft_putstr_fd(t_current->str, 2);
+		ft_putstr_fd(": Permission denied\x1b[0m\n", 2);
+	}
+	free(tmp_abs_path);
+	*exit_status = 1;
+}
+
+//Muestra mensajes de error por el fd 2 y modifica el exit status
+void	ft_error_syntax(int *exit_status, int name, t_token *t_current)
+{
+	if (name == PIPE)
+		ft_putstr_fd \
+		("\033[31mminishell: syntax error near unexpected\
+		 token `|'\x1b[0m\n", 2);
+	else if (name == RED_IN || name == RED_OUT)
+	{
+		if (t_current && (!ft_strncmp(t_current->str, "|", 1) || \
+			!ft_strncmp(t_current->str, "<", 1) \
+			|| !ft_strncmp(t_current->str, ">", 1) || \
+			!ft_strncmp(t_current->str, ">>", 2) || \
+			!ft_strncmp(t_current->str, "<<", 1))) //si es algun meta caracter
+			ft_err_red_metachar(exit_status, name, t_current);
+		else if (t_current)
+		{
+			ft_err_red_errorfile(exit_status, name, t_current);
+			return ;
+		}
+		else
+			ft_putstr_fd("\033[31mminishell: syntax error near \
+			unexpected token `newline'\x1b[0m\n", 2);
+	}
+	*exit_status = 258;
+	return ;
+}
+
+void	ft_error_system(int type)
 {
 	if (type == MALLOC_ERROR)
 		perror("system error -> malloc() error\n");
@@ -25,119 +97,3 @@ void ft_error_system(int type)
 		perror("system error -> close() error\n");
 	exit(type);
 }
-
-//Muestra mensajes de error por el fd 2 y modifica el exit status
-void ft_error_syntax(int *exit_status, int name, t_token *t_current)
-{
-	char *tmp_abs_path;
-
-	if (name == PIPE)
-		ft_putstr_fd("\033[31mminishell: syntax error near unexpected token `|'\x1b[0m\n", 2);
-	else if (name == RED_IN || name == RED_OUT)
-	{
-		if (t_current && (!ft_strncmp(t_current->str, "|", 1) || \
-		 !ft_strncmp(t_current->str, "<", 1) || !ft_strncmp(t_current->str, ">", 1) || \
-		 !ft_strncmp(t_current->str, ">>", 2) || !ft_strncmp(t_current->str, "<<", 1))) //si es algun meta caracter
-		{
-			ft_putstr_fd("\033[31mminishell: syntax error near unexpected token `", 2);
-			if (!ft_strncmp(t_current->str, ">>", 2) || !ft_strncmp(t_current->str, "<<", 2))
-			{
-				ft_putchar_fd(t_current->str[0], 2);
-				ft_putchar_fd(t_current->str[1], 2);
-			}
-			else
-				ft_putchar_fd(t_current->str[0], 2);
-			ft_putstr_fd("'\x1b[0m\n", 2);
-		}
-		else if (t_current /* && (!t_current->next || t_current->next->type != PIPE)*/) //porque la ultima condicion??, suele entrar por error de apertura o < 
-		{
-			tmp_abs_path = getcwd(NULL, 0);
-			if (!tmp_abs_path)
-				ft_error_system(MALLOC_ERROR);
-			tmp_abs_path = ft_strjoin_s(tmp_abs_path, "/");
-			if (!tmp_abs_path)
-				ft_error_system(MALLOC_ERROR);
-			tmp_abs_path = ft_strjoin_s(tmp_abs_path, t_current->str);
-			if (!tmp_abs_path)
-				ft_error_system(MALLOC_ERROR);
-			//ft_putstr_fd(tmp_abs_path, 2); 4 debugg
-			if (access(tmp_abs_path, F_OK) != 0)
-			{
-				ft_putstr_fd("\033[31mminishell: ", 2);
-				ft_putstr_fd(t_current->str, 2);
-				ft_putstr_fd(": No such file or directory\x1b[0m\n", 2);
-			}
-			else
-			{
-				ft_putstr_fd("\033[31mminishell: ", 2);
-				ft_putstr_fd(t_current->str, 2);
-				ft_putstr_fd(": Permission denied\x1b[0m\n", 2);
-			}
-			free(tmp_abs_path);
-			*exit_status = 1;
-			return ;
-		}
-		else
-			ft_putstr_fd("\033[31mminishell: syntax error near unexpected token `newline'\x1b[0m\n", 2);
-	}
-	*exit_status = 258;
-	return ;
-}
-
-//Muestra mensajes de error por el fd 2 y modifica el exit status
-/*int ft_error_syntax(int *exit_status, int name, t_token *t_current)
-{
-	char *tmp_abs_path;
-
-	if (name == PIPE)
-		ft_putstr_fd("\033[31mminishell: syntax error near unexpected token `|'\x1b[0m\n", 2);
-	else if (name == RED_IN || name == RED_OUT)
-	{
-		if (t_current && (!ft_strncmp(t_current->str, "|", 1) || \
-		 !ft_strncmp(t_current->str, "<", 1) || !ft_strncmp(t_current->str, ">", 1) || \
-		 !ft_strncmp(t_current->str, ">>", 2) || !ft_strncmp(t_current->str, "<<", 1))) //si es algun meta caracter
-		{
-			ft_putstr_fd("\033[31mminishell: syntax error near unexpected token `", 2);
-			if (!ft_strncmp(t_current->str, ">>", 2) || !ft_strncmp(t_current->str, "<<", 2))
-			{
-				ft_putchar_fd(t_current->str[0], 2);
-				ft_putchar_fd(t_current->str[1], 2);
-			}
-			else
-				ft_putchar_fd(t_current->str[0], 2);
-			ft_putstr_fd("'\x1b[0m\n", 2);
-		}
-		else if (t_current  && (!t_current->next || t_current->next->type != PIPE)) //porque la ultima condicion??, suele entrar por error de apertura o < 
-		{
-			tmp_abs_path = getcwd(NULL, 0);
-			if (!tmp_abs_path)
-				ft_error_system(MALLOC_ERROR);
-			tmp_abs_path = ft_strjoin_s(tmp_abs_path, "/");
-			if (!tmp_abs_path)
-				ft_error_system(MALLOC_ERROR);
-			tmp_abs_path = ft_strjoin_s(tmp_abs_path, t_current->str);
-			if (!tmp_abs_path)
-				ft_error_system(MALLOC_ERROR);
-			//ft_putstr_fd(tmp_abs_path, 2); 4 debugg
-			if (access(tmp_abs_path, F_OK) != 0)
-			{
-				ft_putstr_fd("\033[31mminishell: ", 2);
-				ft_putstr_fd(t_current->str, 2);
-				ft_putstr_fd(": No such file or directory\x1b[0m\n", 2);
-			}
-			else
-			{
-				ft_putstr_fd("\033[31mminishell: ", 2);
-				ft_putstr_fd(t_current->str, 2);
-				ft_putstr_fd(": Permission denied\x1b[0m\n", 2);
-			}
-			free(tmp_abs_path);
-			*exit_status = 1;
-			return (*exit_status);
-		}
-		else
-			ft_putstr_fd("\033[31mminishell: syntax error near unexpected token `newline'\x1b[0m\n", 2);
-	}
-	*exit_status = 258;
-	return (*exit_status);
-}*/
